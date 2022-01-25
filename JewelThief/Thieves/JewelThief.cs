d285a7e59fd1119051a9b7d600fd92a74e868707
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-25 16:44:04 +0800

    very fonal

--- JewelThief/Thieves/JewelThief.cs
+++ JewelThief/Thieves/JewelThief.cs
@@ -7,9 +7,11 @@ namespace JewelThief
     class JewelThief : Locksmith
     {
         private string stolenJewels;
-        protected override void ReturnContents(string safeContents, SafeOwner owner)
+        protected override void ReturnContents(string safeContents, SafeOwner owner) // owner nggak kepake karena override
+        //protected override void ReturnContents(string safeContents)
         {
             this.stolenJewels = safeContents;
+            //this.stolenJewels = owner.;
             Console.WriteLine($"I'm stealing the jewels! I stole: {stolenJewels}");
         }
     }
